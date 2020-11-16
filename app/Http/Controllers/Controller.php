3a5ba9e6a3f1558f7b52625d307d5ec9aa000128
Marco Marassi <thtg88@users.noreply.github.com>
2020-11-16 15:22:12 +0000

    Removed csv export from base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,20 +20,6 @@ class Controller extends BaseController
     protected $bindings = [];
 
     /**
-     * The csv header row callback implementation.
-     *
-     * @var \Closure
-     */
-    protected $csv_header_row_callback;
-
-    /**
-     * The csv row callback implementation.
-     *
-     * @var \Closure
-     */
-    protected $csv_row_callback;
-
-    /**
      * The service implementation.
      *
      * @var \App\Http\Requests\Contracts\ResourceServiceInterface
@@ -98,45 +84,11 @@ class Controller extends BaseController
     }
 
     /**
-     * Download a listing of all the resources in CSV format.
-     *
-     * @param \App\Http\Requests\DownloadCsvRequest $request
-     * @return \Illuminate\Http\Response
-     */
-    public function downloadCsv(DownloadCsvRequest $request)
-    {
-        // Create download csv helper
-        $helper = new DownloadCsvHelper(
-            $this->service,
-            $this->csv_header_row_callback,
-            $this->csv_row_callback
-        );
-
-        // Get callback
-        $callback = $helper->getCallback($request);
-
-        // Create file name from table name, and timestamp
-        $filename = $this->service->getName().'-'.now()->format('Y-m-d\TH_i_s').'.csv';
-
-        $headers = [
-            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
-            'Content-type' => 'text/csv',
-            'Content-Disposition' => 'attachment; filename='.$filename,
-            'Expires' => '0',
-            'Pragma' => 'public',
-        ];
-
-        // return $callback();
-
-        return response()->stream($callback, 200, $headers);
-    }
-
-    /**
      * Return the service name.
      *
      * @return string
      */
-    protected function getServiceName()
+    protected function getServiceName(): string
     {
         return $this->service->getName();
     }
