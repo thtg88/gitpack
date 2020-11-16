76b4aa1e67297b7a30ef2d22d01884af9e6f6be4
Marco Marassi <thtg88@users.noreply.github.com>
2020-11-16 16:06:57 +0000

    Added create and edit requests contracts

---
+++ app/Http/Requests/Contracts/EditRequestInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Contracts;
+
+interface EditRequestInterface
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool;
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array;
+}
