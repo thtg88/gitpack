c0baa04d54d76d08d0e83ee0bf2c085684c2f1ac
Marco Marassi <thtg88@users.noreply.github.com>
2020-11-17 16:44:35 +0000

    Auth WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,5 @@ import { $$ } from "./helpers/dom";
 var createConfirm = require('./components/confirm').createConfirm;
 
 $$("[data-confirm]").forEach(createConfirm);
-require('alpinejs');
+
+// require('alpinejs');
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], static function () {
+    Route::view('/', 'dashboard')->name('dashboard');
 });
