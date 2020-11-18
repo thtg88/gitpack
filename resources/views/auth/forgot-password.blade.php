016443e5a0ab4ceb1fc9ee3b8846d09f232b7e8e
Marco Marassi <thtg88@users.noreply.github.com>
2020-11-18 13:04:16 +0000

    Add back to login link to forgot password view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -30,7 +30,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-button>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('auth.login') }}">
+                    {{ __('Back to login') }}
+                </a>
+
+                <x-button class="ml-3">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
