d7d16ccc2ada20d4025fe0d821abbe2aee44c597
Marco Marassi <thtg88@users.noreply.github.com>
2021-02-20 14:05:04 +0000

    Added journal entry repository, helper, and singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Helpers\JournalEntryHelper;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register journal entry helper singleton
+        $this->app->singleton('JournalEntryHelper', static function ($app) {
+            return $app->make(JournalEntryHelper::class);
+        });
     }
 
     /**
