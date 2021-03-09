66168bcd61788ded260dd57c6e5d5dd23303022a
Marco Marassi <thtg88@users.noreply.github.com>
2021-03-09 10:14:24 +0000

    Harden return type for remote command handle method

--- app/Jobs/GitRemoteRepository/RemoteCommands/RemoteCommand.php
+++ app/Jobs/GitRemoteRepository/RemoteCommands/RemoteCommand.php
@@ -7,7 +7,7 @@ use Closure;
 
 abstract class RemoteCommand implements RemoteCommandInterface
 {
-    public function handle(Traveler $traveler, Closure $next): mixed
+    public function handle(Traveler $traveler, Closure $next): Traveler
     {
         $traveler->appendCommand($this->getCommand($traveler));
 
