a753e14895de3a6f04287177f7c89151fd4dd275
Marco Marassi <thtg88@users.noreply.github.com>
2021-03-09 15:42:35 +0000

    Make sure to authorize actions from app policy

--- app/Policies/AppPolicy.php
+++ app/Policies/AppPolicy.php
@@ -2,6 +2,68 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
 class AppPolicy extends Policy
 {
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function view(User $user, Model $model)
+    {
+        return $model->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function update(User $user, Model $model)
+    {
+        return $model->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function delete(User $user, Model $model)
+    {
+        return $model->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function restore(User $user, Model $model)
+    {
+        return $model->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param \App\Models\User $user
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return bool
+     */
+    public function forceDelete(User $user, Model $model)
+    {
+        return $model->user_id === $user->id;
+    }
 }
