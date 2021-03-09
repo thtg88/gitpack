1457f40324f5f922ef89a92dcd3da870bb6caa0a
Marco Marassi <thtg88@users.noreply.github.com>
2021-03-09 10:14:05 +0000

    Make sure to specify special cast for rename traveler for write conf remote command

--- app/Jobs/GitRemoteRepository/RemoteCommands/WriteConfRemoteCommand.php
+++ app/Jobs/GitRemoteRepository/RemoteCommands/WriteConfRemoteCommand.php
@@ -2,13 +2,18 @@
 
 namespace App\Jobs\GitRemoteRepository\RemoteCommands;
 
+use App\Jobs\GitRemoteRepository\Travelers\RenameTraveler;
 use App\Jobs\GitRemoteRepository\Travelers\Traveler;
 
 final class WriteConfRemoteCommand extends RemoteCommand
 {
     public function getCommand(Traveler $traveler): string
     {
-        $conf = $traveler->getGitoliteConf();
+        if ($traveler instanceof RenameTraveler) {
+            $conf = $traveler->getNewGitoliteConf();
+        } else {
+            $conf = $traveler->getGitoliteConf();
+        }
 
         return 'echo "'.$conf->output().'" > '.$conf->getConfFilePath();
     }
