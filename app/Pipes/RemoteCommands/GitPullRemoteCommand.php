a4c4b7dd0cb66d43582b34210fb541ecd983064a
Marco Marassi <thtg88@users.noreply.github.com>
2021-06-16 13:30:36 +0100

    Ignore unused variables in git push and pull remote commands

--- app/Pipes/RemoteCommands/GitPullRemoteCommand.php
+++ app/Pipes/RemoteCommands/GitPullRemoteCommand.php
@@ -6,6 +6,7 @@ use App\Travelers\Traveler;
 
 final class GitPullRemoteCommand extends RemoteCommand
 {
+    /** @psalm-suppress ParamNameMismatch */
     public function getCommand(Traveler $_traveler): string
     {
         return 'git pull origin master';
--- app/Pipes/RemoteCommands/GitPushRemoteCommand.php
+++ app/Pipes/RemoteCommands/GitPushRemoteCommand.php
@@ -6,6 +6,7 @@ use App\Travelers\Traveler;
 
 final class GitPushRemoteCommand extends RemoteCommand
 {
+    /** @psalm-suppress ParamNameMismatch */
     public function getCommand(Traveler $_traveler): string
     {
         return 'git push origin master';
